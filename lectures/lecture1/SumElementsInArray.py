7050b8ea36ee0d22481abae467c187e56d9e73a5
nikita <samaraphone@yandex.ru>
2023-09-24 15:02:29 +0300

    add seminar1, modified lecture1

--- lectures/lecture1/SumElementsInArray.py
+++ lectures/lecture1/SumElementsInArray.py
@@ -1,6 +1,15 @@
-### Example 1: loop sum
+# Example 1: loop sum
+# Imperative programming
+
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # объявляем массив arr
 summ = 0                               # объявляем переменную summ равную нулю
 for el in arr:                         # перебираем элементы массива arr
     summ += el                         # на каждом шаге увеличиваем summ на arr
 print(summ)                            # выводим summ в терминал
+
+# Example 2: built-in sum()
+# Declarative programming
+
+arr = [1, 2, 3, 4,
+       5, 6, 7, 8, 9, 10]  # объявляем массив arr
+print(sum(arr))                   # получаем значение функции для аргумента arr и выводим в терминал
